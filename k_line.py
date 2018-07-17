6c9a2fac5ed393ff0762c4e6afb605841012ed3d
jj <39566052@qq.com>
2018-07-17 18:33:59 +0800

    update

--- k_line.py
+++ k_line.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 #import mpl_finance
-from mpl_finance import candlestick_ochl
+from mpl_finance import candlestick_ochl,index_bar
 from datetime import datetime
 import matplotlib.dates as mdates
+from matplotlib.lines import Line2D
+import numpy as np
 
 from framework import frmwk
 import config
@@ -16,29 +18,25 @@ class kline:
         self.ochl = self.fwk.get_K_line(pair, limit=10, dtype="1day") #t o c h l v a
         self.amount = [i.pop() for i in self.ochl]
         self.volume = [i.pop() for i in self.ochl]
-        #self.date = [i.pop(0) for i in self.ochl]
-        #date = [datetime.fromtimestamp(i[0]) for i in self.ochl]
-        #print(date)
-        print(self.ochl)
+        for i in self.ochl:
+            i[0] = mdates.epoch2num(i[0]) #convert timestamp to matplotlib.dates num format
+
+    def graphic(self):
+        self.fig = plt.figure()
+        self.ax1 = self.fig.add_subplot(111) # only one plot
+        #candlestick_ochl(self.ax1, self.ochl, width=0.4, colorup='#77d879', colordown='#db3f3f')
+        #self.ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        #self.ax1.xaxis.set_major_locator(mdates.DayLocator())
+        #self.ax1.grid(True)
+        line1 = Line2D(xdata=[0,1],ydata=[0.5,0.5], color="r")
+        self.ax1.add_line(line1)
+        plt.xticks(rotation=45)
         
-
-    def graph(self, pair):
-        self.gather_data(pair)
-        plt.figure()
-        ax1 = plt.subplot2grid((1,1), (0,0)) # only one grid        
-        candlestick_ochl(ax1, self.ochl) # width=0.8, colorup='#77d879', colordown='#db3f3f')
-        #ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        ax1.xaxis.set_major_locator(mdates.DayLocator())
-        ax1.xaxis_date()
-        ax1.grid(True)
-        plt.xticks(rotation=30)
-        #plt.xlabel('Date')
-        #plt.ylabel('Price')
         plt.show()
-
 
 if __name__ == '__main__':
     config.load_cfg_all()
     pair = config.get_cfg("coin1")+config.get_cfg("coin2")
     kl = kline()
-    kl.graph(pair)
+    kl.gather_data(pair)
+    kl.graphic()
