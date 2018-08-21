298d9d6a677061a4f52102be98697dac01b17683
jj <39566052@qq.com>
2018-08-21 23:47:26 +0800

    update

--- window.py
+++ window.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-from tkinter import *
+#from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from collections import OrderedDict
 
@@ -55,16 +56,15 @@ def trade_graphic(ax, hist):
             elif type == 'open_sell' or type == 'margin_buy' or type == 'loss_buy':
                 cms = 'go'
             ax.plot([time], [price], cms)
-        
 
 class windows:
     def __init__(self):
-        self.win = Tk()
+        self.win = tk.Tk()
         #bind exit method
         self.win.protocol("WM_DELETE_WINDOW", self.exit)
         self.win.bind('<Escape>', lambda e: self.exit())
 
-        self.win.geometry('800x600') #主窗口大小
+        #self.win.geometry('800x600') #主窗口大小
         self.win.title("trade")
         matplotlib.use('TkAgg')
 
@@ -77,10 +77,47 @@ class windows:
         self.win.mainloop()
 
     def layout(self):
-        self.param_select_layout(self.win)
-        self.tab_layout(self.win)
+        f = tk.Frame(self.win)
+        self.param_select_layout(f)
+        f.pack(side=tk.TOP)
+        f = tk.Frame(self.win)
+        self.tab_layout(f)
+        f.pack(side=tk.TOP,fill=tk.BOTH, expand=tk.YES)
 
     def param_select_layout(self, parent):
+        self.indicator_opt = ['bbands','macd', 'stoch']
+        self.plat_opt = ['coinex','okex']
+        self.pair_opt = ['btc_usdt','etc_usdt','eos_usdt','eth_usdt']
+        self._opt = ['1','2']
+        self.add_frame_combobox(parent, self.indicator_opt, self.indicator_select, side=tk.LEFT)
+        self.add_frame_combobox(parent, self.plat_opt, self.plat_select, side=tk.LEFT)
+        self.add_frame_combobox(parent, self.pair_opt, self.pair_select, side=tk.LEFT)
+        self.add_frame_combobox(parent, self._opt, self._select, side=tk.LEFT)
+        pass
+
+    def add_frame_combobox(self, parent, options, func, **params):
+        f = tk.Frame(parent, height=80, width=100)
+        f.pack(**params)
+        comb = ttk.Combobox(f,textvariable=tk.StringVar())
+        comb['values'] = options
+        comb.bind('<<ComboboxSelected>>', func)
+        comb.pack()
+
+    def indicator_select(self, event):
+        self.indicator = event.widget.get()
+        if self.indicator == 'bbands':
+            kl = self.bbands.get_kl()
+        elif self.indicator == 'macd':
+            kl = self.macd.get_kl()
+        self.handle_kline(kl)
+
+    def plat_select(self, event):
+        pass
+
+    def pair_select(self, event):
+        pass
+
+    def _select(self, event):
         pass
 
     def tab_layout(self, parent):
@@ -108,8 +145,8 @@ class windows:
     def tab_analysis_layout(self, parent):
         fig,self.ta_axes = plt.subplots(2,1,sharex=True) #, figsize=(12,8))
         self.ta_canva =FigureCanvasTkAgg(fig, master=parent)
-        self.ta_canva.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
-        self.ta_canva._tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
+        self.ta_canva.get_tk_widget().pack(fill=tk.BOTH, expand=1)
+        self.ta_canva._tkcanvas.pack(fill=tk.BOTH, expand=1)
         toolbar = NavigationToolbar2TkAgg(self.ta_canva, parent)
         toolbar.update()
         #btn = ttk.Button(parent,text='test',command=self.btn_click)
@@ -121,7 +158,7 @@ class windows:
         
     def tab_debug_layout(self, parent):
         self.plat_select_widget(parent)
-        self.debug_label=Label(parent,bg='pink', text='empty')
+        self.debug_label=tk.Label(parent,bg='pink', text='empty')
         self.debug_label.pack()
 
 
@@ -143,12 +180,12 @@ class windows:
         
     def plat_select_widget(self, parent):
         self.plat=("okex","coinex","fcoin")
-        self.platvar = StringVar()
+        self.platvar = tk.StringVar()
         self.platvar.set(0)
 
-        lf=LabelFrame(parent,  text="平台选择")
+        lf=tk.LabelFrame(parent,  text="平台选择")
         for i in range(len(self.plat)):
-            Radiobutton(lf, variable=self.platvar, value=self.plat[i],
+            tk.Radiobutton(lf, variable=self.platvar, value=self.plat[i],
                         text=self.plat[i],indicatoron=0, width=10,
                         command=self.debug_label_update).pack()
         lf.pack()
